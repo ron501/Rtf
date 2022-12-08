9c36d4b18a8da41048d8662107f7698afabf09f4
ron501 <sergey.sokolov72@yandex.ru>
2022-12-08 20:34:24 +0300

    maigen tab

--- app/src/main/java/com/mif/myapplication/MainActivity.kt
+++ app/src/main/java/com/mif/myapplication/MainActivity.kt
@@ -2,10 +2,51 @@ package com.mif.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(MainActivity::class.simpleName, "onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity::class.simpleName, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity::class.simpleName, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause ()
+        Log.d(MainActivity::class.simpleName, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity::class.simpleName, "onStop")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity::class.simpleName, "onRestart")
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(MainActivity::class.simpleName, "onSavedInstanceState")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity::class.simpleName, "onDestroy")
+    }
+}
 
